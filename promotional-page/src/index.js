07ac02bf8776dfd81af94483e25edf49788a98af
Roberto <robertocabezas880@gmail.com>
2022-07-07 20:34:59 -0600

    updates

--- promotional-page/src/index.js
+++ promotional-page/src/index.js
@@ -5,13 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const cors = require('cors');
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-App.use(cors())
+
 
 
 // If you want to start measuring performance in your app, pass a function
